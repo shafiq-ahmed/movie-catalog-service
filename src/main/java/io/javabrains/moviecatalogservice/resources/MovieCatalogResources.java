241b57306c0fb486b628a4e1565ce0651d7d46f5
shafiq-ahmed <shafiqahmd3@gmail.com>
2022-09-29 13:06:40 +0600

    Api call initiated

--- src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResources.java
+++ src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResources.java
@@ -1,18 +1,38 @@
 package io.javabrains.moviecatalogservice.resources;
 
 import io.javabrains.moviecatalogservice.entity.CatalogItem;
+import io.javabrains.moviecatalogservice.entity.Movie;
+import io.javabrains.moviecatalogservice.entity.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResources {
 
+    @Autowired
+    private RestTemplate restTemplate;
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
-        return Collections.singletonList(new CatalogItem("BladeRunner 2049","Action type film",5));
+        //the movies the user has watched and his ratings
+        List<Rating> ratings= Arrays.asList(
+                new Rating("101",4),
+                new Rating("102",2)
+        );
+        //getting the details of those movies through api call
+        return ratings.stream().map(rating -> {
+            //the url that it's going to get the response from and the class that
+            // it's going to map the response to
+            Movie movie = restTemplate.getForObject("http://localhost:9091/movies/"+rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(),"Action type film",rating.getRating());
+        }).collect(Collectors.toList()) ;
+
     }
 }
